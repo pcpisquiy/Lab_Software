5debf05843614d45f1b58727967d19883d71de7c
pcpisquiy <pcpisquiy@gmail.com>
2024-09-10 20:40:57 -0600

    Inicio del repositioro y creacion del web service api

---
+++ LabSoftware/Lab_Software/Controllers/UsuariosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab_Software.DTO;
+namespace Lab_Software.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpGet("ListarUsuarios")]
+        public ActionResult<List<UsuarioDTO>> Get() {
+            return new List<UsuarioDTO>();
+        }
+    }
+}
---
+++ LabSoftware/Lab_Software/DTO/UsuarioDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_Software.DTO
+{
+    public class UsuarioDTO
+    {
+        public string Nombre_Completo { get; set; }
+        public string Correo_Electronico { get; set; }
+        public string Contraseña { get; set; }
+        public string Edad { get; set; }
+        public string Pais { get; set; }
+        public string Numero_de_Telefono { get; set; }
+    }
+}
