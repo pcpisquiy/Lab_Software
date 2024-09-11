991b4bc4e8d6ca8e3b613e617f9b4050bf6fcf27
pcpisquiy <pcpisquiy@gmail.com>
2024-09-10 23:58:47 -0600

    Finalizacion del metodo get Para listar Listas

--- LabSoftware/Lab_Software/Controllers/UsuariosController.cs
+++ LabSoftware/Lab_Software/Controllers/UsuariosController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Lab_Software.DTO;
+using System.IO;
 namespace Lab_Software.Controllers
 {
     
@@ -12,9 +13,20 @@ namespace Lab_Software.Controllers
     [Route("[controller]")]
     public class UsuariosController : ControllerBase
     {
+       private static  List<UsuarioDTO> ListaUsuarios = new List<UsuarioDTO>();
         [HttpGet("ListarUsuarios")]
         public ActionResult<List<UsuarioDTO>> Get() {
             return new List<UsuarioDTO>();
+        }
+        [HttpGet("ObtenerArchivo")]
+        public ActionResult<List<UsuarioDTO>> ObeterArchivo() {
+            StreamReader sr = new StreamReader(Directory.GetCurrentDirectory() + "\\bin\\Debug\\net5.0\\MOCK_DATA.csv");
+            
+            while (!sr.EndOfStream) {
+                ListaUsuarios.Add(new UsuarioDTO(sr.ReadLine()));
+            }
+            sr.Close();
+            return ListaUsuarios;
         }
     }
 }
--- LabSoftware/Lab_Software/DTO/UsuarioDTO.cs
+++ LabSoftware/Lab_Software/DTO/UsuarioDTO.cs
@@ -7,11 +7,51 @@ namespace Lab_Software.DTO
 {
     public class UsuarioDTO
     {
+        public UsuarioDTO()
+        {
+            Nombre_Completo = string.Empty;
+            Correo_Electronico = string.Empty;
+            Contraseña = string.Empty;
+            Edad = 0;
+            Pais = string.Empty;
+            Numero_de_Telefono = string.Empty;
+        }
+        public UsuarioDTO(string Data)
+        {
+            string[] TuplaUsuario = Data.Split(',');
+            Nombre_Completo = TuplaUsuario[0];
+            Correo_Electronico = TuplaUsuario[1];
+            Contraseña = TuplaUsuario[2];
+            Edad = int.Parse(TuplaUsuario[3]);
+            Pais = TuplaUsuario[4];
+            Numero_de_Telefono = TuplaUsuario[5];
+        }
+
+        /// <summary>
+        /// Obtiene o establece el nombre completo del usuario.
+        /// </summary>
         public string Nombre_Completo { get; set; }
+
+        /// <summary>
+        /// Obtiene o estblece el correo electrónico del usuario.
+        /// </summary>
         public string Correo_Electronico { get; set; }
+
+        /// <summary>
+        ///  Obtiene o estblece la contraseña del usuario.
+        /// </summary>
         public string Contraseña { get; set; }
-        public string Edad { get; set; }
+        /// <summary>
+        /// Obtiene o estblece la edad del usuario.
+        /// </summary>
+        public int Edad { get; set; }
+        /// <summary>
+        /// Obtiene o estblece el Pais del usuario.
+        /// </summary>
         public string Pais { get; set; }
+        /// <summary>
+        /// Obtiene o estblece el Numero de Telefono del usuario.
+        /// </summary>
         public string Numero_de_Telefono { get; set; }
     }
 }
